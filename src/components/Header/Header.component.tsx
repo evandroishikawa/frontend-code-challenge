28202e2b0c456d6d1c7de91c04216b3ba1f6a2fd
Evandro Ishikawa <evandro.ishikawa@sympla.com.br>
2026-08-13 11:31:23 -0300

    add basic structure

---
+++ src/components/Header/Header.component.tsx
@@ -0,0 +1,9 @@
+// type HeaderProps = {}
+
+const Header = () => {
+  return (
+    <header className="bg-white shadow-sm flex justify-center items-center py-4">Hello World</header>
+  )
+}
+
+export default Header
